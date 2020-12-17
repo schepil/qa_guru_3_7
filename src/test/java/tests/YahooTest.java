27cbde47192920d5a75b00ea14cf509937d45b4b
Дранкович Светлана <svetlanadrankovich789@gmail.com>
2020-12-18 00:08:36 +0300

    add YandexTests and YahooTest

---
+++ src/test/java/tests/YahooTest.java
@@ -0,0 +1,21 @@
+package tests;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class YahooTest {
+    @Test
+    void selenideSearchTest() {
+        // Открыть Yahooo
+        open("https://ru.search.yahoo.com");
+
+        // Ввести Selenide в поиск
+        $("#yschsp").setValue("Selenide").pressEnter();
+
+        // Проверить, что Selenide появился в результатах поиска
+        $("html").shouldHave(Condition.text("selenide.org"));
+    }
+}
---
+++ src/test/java/tests/YandexTests.java
@@ -0,0 +1,22 @@
+package tests;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class YandexTests {
+
+     @Test
+    void selenideSearchTest() {
+        // Открыть Yandex
+        open("https://yandex.ru");
+
+        // Ввести Selenide в поиск
+        $("#text").setValue("Selenide").pressEnter();
+
+        // Проверить, что Selenide появился в результатах поиска
+        $("html").shouldHave(Condition.text("selenide.org"));
+    }
+}
